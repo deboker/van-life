66c09def7ea1d0f766251ce6a68efc10c968b4e4
deboker <117391017+deboker@users.noreply.github.com>
2023-07-31 16:03:15 +0200

    ch

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -36,12 +36,12 @@ export default function Login() {
         <div className="login-container">
             {
                 location.state?.message &&
-                <h3 className="login-first">{location.state.message}</h3>
+                <h3 className="login-error">{location.state.message}</h3>
             }
             <h1>Sign in to your account</h1>
             {
                 error?.message &&
-                <h3 className="login-first">{error.message}</h3>
+                <h3 className="login-error">{error.message}</h3>
             }
             <form onSubmit={handleSubmit} className="login-form">
                 <input
