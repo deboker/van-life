629fac8b652d9d00f1ff15be79718bda36ccb6b6
deboker <117391017+deboker@users.noreply.github.com>
2026-03-11 23:58:32 +0100

    new storage bucket key

--- api.js
+++ api.js
@@ -33,10 +33,8 @@ const firebaseConfig = {
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket:
-    (import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "").replace(
-      ".firebasestorage.app",
-      ".appspot.com"
-    ) || `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.appspot.com`,
+    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ||
+    `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.appspot.com`,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
