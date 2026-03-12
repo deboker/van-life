f736c1ef0c0928d060834cc96dc3ba0d335feee1
deboker <117391017+deboker@users.noreply.github.com>
2026-03-12 12:54:41 +0100

    logo footer and head

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/images/vanGo_logo.webp";
+import logo from "../assets/images/vanGo_icon.webp";
 
 export default function Footer() {
   return (
